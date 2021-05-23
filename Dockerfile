662d45484ed80f22298fc02838cf7693be0fe882
Daniil <appendme@yandex.ru>
2021-05-23 11:56:32 +0700

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+FROM gcc:10.2.0 AS vcpkg-env
+
+RUN apt-get update && \
+    apt-get install -y git curl unzip tar && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN git clone --depth 1 https://github.com/microsoft/vcpkg /backend/vcpkg
+RUN /backend/vcpkg/bootstrap-vcpkg.sh -disableMetrics
+RUN ln -s /backend/vcpkg/vcpkg /usr/bin/vcpkg
+
+
+FROM vcpkg-env AS backend-build
+
+RUN apt-get install -y cmake zip libgl-dev && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN vcpkg install --clean-after-build restinio
+RUN rm -rf \
+        /backend/vcpkg/buildtrees \
+        /backend/vcpkg/downloads
+
+ADD ./backend /backend/src
+WORKDIR /backend/src/build
+
+RUN cmake .. -DCMAKE_TOOLCHAIN_FILE=/backend/vcpkg/scripts/buildsystems/vcpkg.cmake && \
+    cmake --build .
+
+
+FROM ubuntu:20.04
+
+RUN groupadd -r ape && useradd -r -g ape ape
+USER ape
+
+COPY --from=backend-build /backend/src/build/backend /backend/
+
+ENTRYPOINT [ "./backend/backend" ]
---
+++ backend/app.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <restinio/all.hpp>
+
+namespace rr = restinio::router;
+using router_t = rr::express_router_t<>;
+
+auto handler() {
+	auto router = std::make_unique<router_t>();
+
+	router->http_get("/", [](auto req, auto params) {
+		constexpr std::string_view message =
+			"<html><body>"
+			"Hello world"
+			"</body></html>";
+
+		req->create_response().set_body(message).done();
+
+		return restinio::request_accepted();
+	});
+
+	router->non_matched_request_handler([](auto req) {
+		return req->create_response(restinio::status_not_found())
+				.append_header_date_field()
+				.connection_close()
+				.done();
+	});
+
+	return router;
+}
+
+int main()
+{
+	using traits_t =
+	  restinio::traits_t<
+		 restinio::asio_timer_manager_t,
+		 restinio::single_threaded_ostream_logger_t,
+		 router_t>;
+
+	try {
+		restinio::run(
+		restinio::on_this_thread<traits_t>()
+			.port(25080)
+			.address("0.0.0.0")
+			.request_handler(handler()));
+	} catch(const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+	
+	return 0;
+}
